0fe202b2b437ef66b4cf9ab0ec75e5fd818c85c3
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-01-24 19:54:47 -0500

    Define routes and display character listing

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { Card, CardBody, CardTitle, CardSubtitle, Col, CardImg} from "reactstrap";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+function CharacterCard({character}) {
+  return (  
+    <Col>
+      <Card className="card-border" >  
+          <CardBody className="card-body">
+          <CardTitle>{character.name}</CardTitle> 
+            <CardSubtitle>Status: {character.status}</CardSubtitle>
+            <CardSubtitle>Gender: {character.gender}</CardSubtitle>
+            <CardImg top width="100%" src={character.image} alt="Card image cap" />
+          </CardBody>
+      </Card>
+    </Col>    
+  );
+  
 }
+
+export default CharacterCard;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,66 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, Row, Button } from "reactstrap";
+import CharacterCard from './CharacterCard';
 
-export default function CharacterList() {
+
+function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState('');
+  const [filterCharacters, setFilterCharacters] = useState('');
 
+  const apiUrl = 'https://rickandmortyapi.com/api/character/';
+ 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    // TODO: Add API Request here - must run in useEffect
+    //  Important: verify the 2nd useEffect parameter: the dependancies array!
+    charactersApi(apiUrl);
   }, []);
+
+  const charactersApi = (apiUrl) => {
+    axios
+        .get(apiUrl)
+        // Which we then set to state
+        .then(res => {
+          
+          setFilterCharacters(res.data.results);
+          setCharacters(res.data);
+        })
+        // Always include error handling
+        .catch(err => console.log(err));
+  };
+
+  let showCharacters = function(){};
+  let nextCharacters = function(){};
+  let previousCharacters = function(){};
+  
+  if(characters !== '')
+  {
+    showCharacters = () => {
+      let charactersList = [];
+      filterCharacters.map(character => {
+        charactersList.push(<CharacterCard key={character.id} character={character} />);
+        return null;
+      });
+      return charactersList;
+    }
+
+   
+  }
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+    <div>
+      
+      
+       <Container>
+       <Row>
+        {showCharacters()}
+       </Row>
+      </Container>
+    </div>
     </section>
   );
 }
+
+export default CharacterList;
